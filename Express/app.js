cd369433232ee736f6f5c98766bdd651e841ad23
Kabira Singh <yogesh.baghel86@gmail.com>
2021-07-11 22:03:54 +0530

    experimenting with express

--- Express/app.js
+++ Express/app.js
@@ -11,7 +11,7 @@ app.use((req, res, next) => {
 
 app.use((req, res, next) => {
     console.log('In another middleware!');
-    res.send('<h1>Hello from Express!</h1>');
+    res.send(302);
 });
 
 const server = http.createServer(app);
