5697da180a11603a1a2cbc888ad6ddd4294d829c
Kabira Singh <yogesh.baghel86@gmail.com>
2021-07-17 14:07:59 +0530

    Conected the databse with mongo atlas

--- File-Sharing App/Backend/config/db.js
+++ File-Sharing App/Backend/config/db.js
@@ -1,13 +1,14 @@
+require('dotenv').config();
 const mongoose=require('mongoose');
 
 function connectDB(){
     //database connection
-    mongoose.connect('url',{ useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false});
+    mongoose.connect(process.env.MONGO_CONNECTION_URL,{ useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false});
     const connection=mongoose.connection;
 
     //For Successfull Connection
     connection.once('open',function(){
-        console.log("Succesfull Connection!");
+        console.log("Databsse Succesfully Connection!");
     }).catch(err=>{
         console.log('Connection failed');
     })
--- File-Sharing App/Backend/server.js
+++ File-Sharing App/Backend/server.js
@@ -2,7 +2,8 @@ const express=require('express');
 const app=express();
 
 const PORT=process.env.PORT ||3000;
-
+const connectDB=require('./config/db');
+connectDB();
 app.listen(PORT,()=>{
     console.log(`Listening on port: ${PORT}`);
 })
