02ab951128914f5000c1e52f5ce7fa3b6251a3c0
Kabira Singh <yogesh.baghel86@gmail.com>
2021-07-17 18:34:14 +0530

    installed uuid for unique id of download link

--- File-Sharing App/Backend/routes/files.js
+++ File-Sharing App/Backend/routes/files.js
@@ -1,6 +1,8 @@
 const router=require('express').Router();
 const multer=require('multer');
 const path=require('path');
+const File=require('../models/file');
+const {v4:uuid4}=require('uuid');
 
 let storage = multer.diskStorage({
     destination:(req,file,cb)=>cb(null,'uploads/'),
@@ -10,16 +12,36 @@ let storage = multer.diskStorage({
     }
 })
 
-let upload=multer({ storage: storage });
+let upload=multer({ 
+    storage: storage,
+    limit: {fileSize:1000000 * 100} //100mb
+ }).single('myfile');   //for uploading single file
 
-router.post('/',(req,res)=>{
+router.post('/',async (req,res)=>{
     //validate request
     if(!req.file){
         return res.json({error:'All fields are required'});
     }
 
     //Store file
+    upload(req,res,(err)=>{
+        if(err){
+            console.log(err);
+            return res.status(500).json({error:err.message});
+        }
+    })
+    
     //Store into Database
+    const file= new File({
+        filename:req.filename,
+        uuid: uuid4(),
+        path: req.file.path,  //mix of destination and file name
+        size: req.file.size
+    });
+
+    const reponse= await file.save();
+    return res.json({file:`${process.env.APP_BASE_URL}/files/${response.uuid}`});
+    //http://localhost:8000/files/232342jdfdf
     //Response->Link
 });
 
