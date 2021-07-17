f00b6327acd3057614e092d4f2e2aeafc6b9df81
Kabira Singh <yogesh.baghel86@gmail.com>
2021-07-17 13:00:23 +0530

    Pausing the express

--- Express/app.js
+++ Express/app.js
@@ -4,14 +4,21 @@ const express = require('express');
 
 const app = express();
 
-app.use((req, res, next) => {
-    console.log('In the middleware!');
-    next(); // Allows the request to continue to the next middleware in line
+// app.use((req, res, next) => {
+//     console.log('In the middleware!');
+//     next(); // Allows the request to continue to the next middleware in line
+// });
+
+app.use('/add-product',(req, res, next) => {
+    console.log('In another middleware!');
+    res.send('<h1>Add product page</h1>');
+    // res.send(302);
 });
 
-app.use((req, res, next) => {
+app.use('/',(req, res, next) => {
     console.log('In another middleware!');
-    res.send(302);
+    res.send('<h1>Home page</h1>')
+    // res.send(302);
 });
 
 const server = http.createServer(app);
