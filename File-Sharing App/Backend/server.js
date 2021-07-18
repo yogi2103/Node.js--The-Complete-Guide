5e149f46c5254e9f21ba6cd0b33e47bec1fdd1d6
Kabira Singh <yogesh.baghel86@gmail.com>
2021-07-18 11:33:08 +0530

    done with download endpoint

--- File-Sharing App/Backend/server.js
+++ File-Sharing App/Backend/server.js
@@ -17,7 +17,7 @@ app.set('view engine','ejs');
 //Routes
 app.use('/api/files',require('./routes/files'));
 app.use('/files',require('./routes/show'));
-app.use('/files/download',require('./'))
+app.use('/files/download',require('./routes/download'));
 
 app.listen(PORT,()=>{
     console.log(`Listening on port: ${PORT}`);
