dd0ff8c126c30873403f5bb32dbb6870ff3d047c
Kabira Singh <yogesh.baghel86@gmail.com>
2021-07-18 12:45:48 +0530

    Beginning with SMTP setup

--- File-Sharing App/Backend/Services/emailServies.js
+++ File-Sharing App/Backend/Services/emailServies.js
@@ -1,7 +1,8 @@
 const nodemailer = require('nodemailer');
 
 function sendMail({from, To, subject, text, html}){
-
+    let transporter= nodemailer.createTransport({
+    })
 }
 
 module.exports=sendMail;
