bdb79cd4a2cd06cc0f3bcbb8afee03ef61f53be2
Kabira Singh <yogesh.baghel86@gmail.com>
2021-07-24 22:53:13 +0530

    Added drag and drop class using js

--- File-Sharing App/Frontend/index.js
+++ File-Sharing App/Frontend/index.js
@@ -1,6 +1,19 @@
 const dropZone= document.querySelector(".drop-zone");
 
+//for adding the class dragged when an item is dragged
 dropZone.addEventListener("dragover",(e)=>{
-    console.log('Dragging');
-    dropZone.classList.add("dragged");
+    e.preventDefault();
+    if(!dropZone.classList.contains("dragged")){
+        dropZone.classList.add("dragged");   
+    }
+});
+
+//for removing the class dragged
+dropZone.addEventListener("dragleave",(e)=>{
+    dropZone.classList.remove("dragged");
+});
+
+dropZone.addEventListener("drop",(e)=>{
+    e.preventDefault();
+    dropZone.classList.remove("dragged");
 });
