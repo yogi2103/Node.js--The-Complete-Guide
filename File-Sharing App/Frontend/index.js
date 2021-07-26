09fd1899501ee30f2dca76dea1d6798475a55a5a
Kabira Singh <yogesh.baghel86@gmail.com>
2021-07-26 20:41:07 +0530

    made function named upload file

--- File-Sharing App/Frontend/index.js
+++ File-Sharing App/Frontend/index.js
@@ -1,5 +1,6 @@
 const dropZone= document.querySelector(".drop-zone");
-
+const browseBtn= document.querySelector(".browseBtn");
+const fileInput=document.querySelector("#fileinput");
 //for adding the class dragged when an item is dragged
 dropZone.addEventListener("dragover",(e)=>{
     e.preventDefault();
@@ -16,4 +17,23 @@ dropZone.addEventListener("dragleave",(e)=>{
 dropZone.addEventListener("drop",(e)=>{
     e.preventDefault();
     dropZone.classList.remove("dragged");
+    console.log(e.dataTransfer.files.length);
+    const files=e.dataTransfer.files;    
+    if(files.length>0){
+        fileInput.files=files;
+    }
 });
+
+browseBtn.addEventListener("click",()=>{
+    fileInput.click();
+})
+
+const uploadFile=()=>{
+    const file= fileInput.files[0];
+    const formData= new formData();
+    formData.append(file);
+    const xhr= new XMLHttpRequest();
+    xhr.onreadystatechange = ()=>{
+        
+    }
+}
